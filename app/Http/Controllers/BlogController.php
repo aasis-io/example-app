21834a7f29fe157774fcfa98ab7741d985ceffe2
Ashish Thapa <heyashishthapa@gmail.com>
2024-05-28 13:17:01 +0545

    ORM and Equilent

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
@@ -12,10 +13,40 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blog = Blog::all();
+        // $blog = Blog::all();         // $blog = DB::select('SELECT * FROM blogs');
 
-        return $blog;
+
+
+
+        //create data
+        // $blog = new Blog();
+        // $blog->title = 'this is a new title 2';
+        // $blog->body = 'this is a new body 2';
+        // $blog->status = 0;
+        // $blog->save();
+
+        //Update data
+        // $blog = Blog::find(2);
+        // $blog->title = 'this is updated one';
+        // $blog->body = 'this is the updated body';
+        // $blog->status = 1;
+        // $blog->save();
+
+        // //get data
+        // $blog = Blog::where(['status' => 0, 'id' => 1])->get();
+
+        //delete data
+        $blog = Blog::FindOrFail(2);
+        $blog->delete();
+
+        dd($blog);
+        // return $blog;
     }
+    // public function index()
+    // {
+    //     $blog = Blog::find(2);
+    //     return $blog;
+    // }
 
     /**
      * Show the form for creating a new resource.
